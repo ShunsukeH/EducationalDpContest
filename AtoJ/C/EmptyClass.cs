54dfa5359aaf8aaaea8e2ca3b8b3554c76d959ef
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-02-03 23:14:18 +0900

    D

--- AtoJ/C/EmptyClass.cs
+++ AtoJ/C/EmptyClass.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Linq;
+
 namespace C
 {
-    public class EmptyClass
+    class Program
     {
-        public EmptyClass()
+        static void Main(string[] args)
         {
+
+            int n = int.Parse(Console.ReadLine());
+            var act = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                act[i] = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            }
+            long[][] dp = new long[n + 1][];
+            dp = dp.Select(e => e = new long[] { 0, 0, 0 }).ToArray();
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < act[i - i].Length; j++)
+                {
+                    for (int k = 0; k < act[i - 1].Length; k++)
+                    {
+                        if (k == j) continue;
+                        changeMax(ref dp[i][k], dp[i - 1][j] + act[i - 1][k]);
+                    }
+                }
+            }
+            long res = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                changeMax(ref res, dp[n][i]);
+            }
+            Console.WriteLine(res);
+        }
+
+        static bool changeMax(ref long a, long b)
+        {
+            if (a < b)
+            {
+                a = b;
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ AtoJ/D/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int[][] wv = new int[nk[0]][];
+            for (int i = 0; i < nk[0]; i++)
+            {
+                wv[i] = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            }
+
+            long[][] dp = new long[nk[0] + 1][];
+            dp = dp.Select(e => e = new long[100001]).ToArray();
+            dp = dp.Select(e => e.Select(elem => elem = 0).ToArray()).ToArray();
+            for (int i = 0; i < nk[0]; i++)
+            {
+                for (int sumW = 0; sumW <= nk[1]; sumW++)
+                {
+                    if (sumW - wv[i][0] >= 0)
+                    {
+                        changeMax(ref dp[i + 1][sumW], dp[i][sumW - wv[i][0]] + wv[i][1]);
+                    }
+                    changeMax(ref dp[i + 1][sumW], dp[i][sumW]);
+                }
+            }
+            Console.WriteLine(dp[nk[0]][nk[1]]);
+        }
+
+        static bool changeMax(ref long a, long b)
+        {
+            if (a < b)
+            {
+                a = b;
+                return true;
+            }
+            return false;
+        }
+    }
+}
